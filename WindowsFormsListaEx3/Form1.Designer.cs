bcfeec6a63a27fc0abb06e7f454366d70c93dcd5
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2025-01-16 20:10:29 -0300

    Add files via upload

---
+++ WindowsFormsListaEx3/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ListaPooFormsExer3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calcular = new System.Windows.Forms.Button();
+            this.Diagonal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Calcular
+            // 
+            this.Calcular.Location = new System.Drawing.Point(302, 206);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(75, 26);
+            this.Calcular.TabIndex = 0;
+            this.Calcular.Text = "Area";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.Area_diago);
+            // 
+            // Diagonal
+            // 
+            this.Diagonal.Location = new System.Drawing.Point(287, 176);
+            this.Diagonal.Name = "Diagonal";
+            this.Diagonal.Size = new System.Drawing.Size(100, 20);
+            this.Diagonal.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Diagonal";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(339, 252);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(13, 13);
+            this.Resultado.TabIndex = 3;
+            this.Resultado.Text = "0";
+            this.Resultado.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Diagonal);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calcular;
+        private System.Windows.Forms.TextBox Diagonal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsListaEx3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaPooFormsExer3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Area_diago(object sender, EventArgs e)
+        {
+            double dia, resul;
+            dia = double.Parse(Diagonal.Text);
+            resul = (dia * dia) / 2;
+            Resultado.Text = resul.ToString();
+        }
+    }
+}
