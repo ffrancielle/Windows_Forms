1d737d22b9e84d626a7e1f335d8ce30219e363a7
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2025-01-16 20:13:10 -0300

    Add files via upload

---
+++ WindowsFormsListaEx4/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ListaPooFormsExer4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Base = new System.Windows.Forms.TextBox();
+            this.Altura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Base
+            // 
+            this.Base.Location = new System.Drawing.Point(190, 168);
+            this.Base.Name = "Base";
+            this.Base.Size = new System.Drawing.Size(100, 20);
+            this.Base.TabIndex = 0;
+            // 
+            // Altura
+            // 
+            this.Altura.Location = new System.Drawing.Point(372, 168);
+            this.Altura.Name = "Altura";
+            this.Altura.Size = new System.Drawing.Size(100, 20);
+            this.Altura.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Base";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resultado:";
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(319, 286);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(13, 13);
+            this.Resultado.TabIndex = 5;
+            this.Resultado.Text = "0";
+            // 
+            // Calcular
+            // 
+            this.Calcular.Location = new System.Drawing.Point(289, 216);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(75, 23);
+            this.Calcular.TabIndex = 6;
+            this.Calcular.Text = "Area";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.Area_Triangulo);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 393);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Altura);
+            this.Controls.Add(this.Base);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Base;
+        private System.Windows.Forms.TextBox Altura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.Button Calcular;
+    }
+}
+
---
+++ WindowsFormsListaEx4/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaPooFormsExer4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Area_Triangulo(object sender, EventArgs e)
+        {
+            double alt, bas, are;
+            bas = double.Parse(Base.Text);
+            alt = double.Parse(Altura.Text);
+            are = (bas * alt) / 2;
+            Resultado.Text = are.ToString();
+        }
+    }
+}
