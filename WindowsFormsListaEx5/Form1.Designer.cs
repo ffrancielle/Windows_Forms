e12471c841686792908293288887892dafa68509
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2025-01-16 20:15:01 -0300

    Add files via upload

---
+++ WindowsFormsListaEx5/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ListaPooFormsExer5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Milhas = new System.Windows.Forms.TextBox();
+            this.Converter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Milhas
+            // 
+            this.Milhas.Location = new System.Drawing.Point(318, 174);
+            this.Milhas.Name = "Milhas";
+            this.Milhas.Size = new System.Drawing.Size(100, 20);
+            this.Milhas.TabIndex = 0;
+            // 
+            // Converter
+            // 
+            this.Converter.Location = new System.Drawing.Point(333, 210);
+            this.Converter.Name = "Converter";
+            this.Converter.Size = new System.Drawing.Size(75, 23);
+            this.Converter.TabIndex = 2;
+            this.Converter.Text = "Converter";
+            this.Converter.UseVisualStyleBackColor = true;
+            this.Converter.Click += new System.EventHandler(this.Converte_pKm);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Milhas maritimas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado:";
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(361, 257);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(13, 13);
+            this.Resultado.TabIndex = 5;
+            this.Resultado.Text = "0";
+            this.Resultado.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Converter);
+            this.Controls.Add(this.Milhas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Milhas;
+        private System.Windows.Forms.Button Converter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Resultado;
+    }
+}
+
---
+++ WindowsFormsListaEx5/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaPooFormsExer5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Converte_pKm(object sender, EventArgs e)
+        {
+            Double milha, resul;
+            milha = double.Parse(Milhas.Text);
+            resul = milha * 1.852;
+            Resultado.Text = resul.ToString() + "Km";
+        }
+    }
+}
